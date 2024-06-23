41b0165399195d54885010edea0adbeb257006fd
pengliubo <77919330+pengliubo@users.noreply.github.com>
2024-06-24 01:35:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.51kxg.com": dockerHub,
+  "quay.51kxg.com": "https://quay.io",
+  "gcr.51kxg.com": "https://gcr.io",
+  "k8s-gcr.51kxg.com": "https://k8s.gcr.io",
+  "k8s.51kxg.com": "https://registry.k8s.io",
+  "ghcr.51kxg.com": "https://ghcr.io",
+  "cloudsmith.51kxg.com": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.51kxg.com": dockerHub,
 };
 
 function routeByHosts(host) {
